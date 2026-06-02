82c5fca7ba43c4e987833cae2b538229544ea50f
HiqueSteves <102489986+HiqueSteves@users.noreply.github.com>
2026-06-02 16:53:56 -0300

    Add files via upload

---
+++ Aula 153 a 156/PrimeiroProjetoJava/src/inicial/App.java
@@ -0,0 +1,21 @@
+package inicial;
+
+public class App {
+
+	public static void main(String[] args) {
+		String nome = ("Henrique");
+		int idade = 19;
+		
+		System.out.println("Olá, Mundo!");
+		System.out.print("Seja bem vindo " + nome + ". ");
+		System.out.print("Essa é sua primeira aula de Java!\n\n");
+		
+		System.out.printf("Meu nome é %s e tenho %d anos. \n\n", nome, idade);
+		
+		double pi = 3.1415926536;
+		
+		System.out.printf("O valor de pi é %.2f", pi);
+
+	}
+
+}
